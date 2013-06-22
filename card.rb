8399c67aca110d807e267aee1b9ac5b3bf062ed8
Steven Wilkin <stevenwilkin@gmail.com>
2013-06-22 12:32:29 +0100

    accept the card's name when constructing

--- card.rb
+++ card.rb
@@ -1,2 +1,4 @@
 class Card
+  def initialize(name)
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,4 +1,14 @@
 require "spec_helper"
 
 describe Card do
+  describe ".new" do
+    let(:suit) { "C" }
+    let(:value) { "7" }
+    let(:name) { "#{value}#{suit}" }
+    let(:card) { Card.new(name) }
+
+    it "creates a new card" do
+      card.should be_a Card
+    end
+  end
 end
