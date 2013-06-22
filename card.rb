9b0de9cafacead6542d8300a81b37f2c98145920
Steven Wilkin <stevenwilkin@gmail.com>
2013-06-22 12:35:38 +0100

    set the attributes of a new Card

--- card.rb
+++ card.rb
@@ -1,4 +1,9 @@
 class Card
+  attr_accessor :name, :value, :suit
+
   def initialize(name)
+    @name = name
+    @value = name[0]
+    @suit = name[1]
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,5 +10,17 @@ describe Card do
     it "creates a new card" do
       card.should be_a Card
     end
+
+    it "sets the name" do
+      card.name.should == name
+    end
+
+    it "sets the value" do
+      card.value.should == value
+    end
+
+    it "sets the suit" do
+      card.suit.should == suit
+    end
   end
 end
